585db8a9d01c3fb221c0135e133c51e78628a5c6
mahadkhan17030 <muhammadmahadkhan80@gmail.com>
2024-02-03 15:38:50 +0500

    work completed

---
+++ app.js
@@ -0,0 +1,5 @@
+var display=document.getElementById("displayinput")
+function getVal(val){display.value += val}
+function nill(){display.value=""}
+function calculate(){
+display.value=eval(display.value)}
